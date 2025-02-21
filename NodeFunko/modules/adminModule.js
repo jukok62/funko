f772904154a3ee19185d7c8bb68088af74b921e7
jukok62 <julien.coquerie@hotmail.fr>
2025-02-21 12:08:38 +0100

    admin service finished

--- NodeFunko/modules/adminModule.js
+++ NodeFunko/modules/adminModule.js
@@ -4,22 +4,24 @@ const admin = require ('../services/admin')
 const path = require('path');
 
 
+
+
 router.post("/addPop", async (req, res) => {
     try {
         const produit = req.body;
-        console.log("clg de produit ", produit);
+
+        const nameFile = path.basename(produit.pop.photo);
+        produit.pop.photo = nameFile;
 
          // Ajouter la pop et récupérer son insertId
          const resultProduit = await admin.AddProduit(produit.pop);
          const fkPop = resultProduit.insertId; // Récupération de l'ID du produit inséré
 
-         console.log("ID de la pop insérée :", fkPop);
-
          // Ajouter l'ID du produit à l'image
-        produit.image.fk_pop = fkPop;
+        produit.pop.fk_Pop = fkPop;
 
          // Ajouter l'image avec la bonne clé étrangère
-         const resultImage = await admin.AddPicture(produit.image);
+         const resultImage = await admin.AddPicture(produit);
 
         res.json({ 
             message: "Ajout réussi",
@@ -33,9 +35,39 @@ router.post("/addPop", async (req, res) => {
     }
 });
 
+
+router.patch('/', async (req, res) => {
+    try {
+        const produit = req.body
+        console.log('clg de produit dans la modification', produit);
+
+        const nameFile = path.basename(produit.pop.photo);
+
+        // console.log('clg de nameFile : ', nameFile);
+        produit.pop.photo = nameFile;
+        // Modifier la Pop
+        const resultProduit = await admin.modifyPop(produit.pop)
+
+         // Ajouter l'image avec la bonne clé étrangère
+         const resultImage = await admin.modifyPhoto(produit);
+         console.log('clg de resultImage : ', resultImage);
+
+        res.json({ 
+            message: "Ajout réussi",
+            produit: resultProduit,
+            image: resultImage
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erreur lors de la modification" });
+    }
+})
+
+
+
 router.delete("/deletePop/:id", async (req, res) => {
     const popId = req.params.id;
-    console.log(popId);
 
     try {
         await admin.DeletePictureById(popId);
@@ -49,14 +81,28 @@ router.delete("/deletePop/:id", async (req, res) => {
     }
 })
 
-router.get('/license'), (req, res) => {
-    admin.getLicense().then((result) =>{
+router.get("/license", async (req, res) => {
+
+    admin.getLicense().then((result) => {
         res.json(result);
-    }).catch((error) =>{
-        console.log(error);
-        res.json({message : "une erreur est survenue"})
+    }).catch((err) => {
+        console.log(err);
+        res.status(404).json({ message: "license not found" });
     })
-}
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    admin.getPopById(id).then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(404).json({ message: "Pop introuvable" });
+    })
+})
+
+
 
 
 
--- NodeFunko/services/admin.js
+++ NodeFunko/services/admin.js
@@ -5,7 +5,7 @@ const AddProduit = (produit) => {
         let sql = `INSERT INTO pop (Name_Pop, Price_Pop, Height_Pop, Special_Pop, 
             Chase_Pop, Date_Pop, Number_Pop, FK_License) 
             VALUES (?,?,?,?,?,?,?,?);`
-        conn.query(sql, [produit.name, produit.price, produit.height, produit.special, 
+        conn.query(sql, [produit.namePop, produit.price, produit.height, produit.special, 
                 produit.chase, produit.date, produit.number, produit.license], 
                         (err,result) => {
             if (err) {
@@ -16,11 +16,12 @@ const AddProduit = (produit) => {
     });
 }
 
+
 const AddPicture = (image) => {
     return new Promise((resolve, reject) => {
         let sql = `INSERT INTO pictures (Main_Picture, Second_Picture, Other_Picture, FK_Pop) 
             VALUES (?,?,?,?);`
-        conn.query(sql, [image.main_picture, image.second_picture, image.third_picture, image.fk_pop], 
+        conn.query(sql, [image.pop.photo, image.second_picture, image.third_picture, image.pop.fk_Pop], 
                         (err,result) => {
             if (err) {
                 reject(err);
@@ -32,7 +33,7 @@ const AddPicture = (image) => {
 
 const DeletePictureById = (id) => {
     return new Promise ((resolve, reject) => {
-        let sql = `DELETE FROM pictures WHERE FK_Pop =?`
+        let sql = `DELETE FROM pictures WHERE FK_Pop = ?`
         conn.query(sql, [id], (err, result) => {
             if (err) {
                 return reject(err)
@@ -66,10 +67,55 @@ const getLicense = () => {
     })
 }
 
+const getPopById = (id) => {
+    return new Promise((resolve, reject) => {
+        let sql = `SELECT * FROM pop 
+        JOIN pictures on FK_Pop = ID_Pop
+        WHERE ID_Pop = ?;`
+        conn.query(sql, [id], (err, result) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve(result[0]);
+        })
+    })
+}
+
+const modifyPop = (pop) => {
+    return new Promise((resolve, reject) => {
+        let sql = `UPDATE pop SET Name_Pop = ?, Price_Pop = ?, Height_Pop = ?, Special_Pop = ?, 
+            Chase_Pop = ?, Date_Pop = ?, Number_Pop = ?, FK_License = ? WHERE ID_Pop = ?`
+        conn.query(sql, [pop.namePop, pop.price, pop.height, pop.special, 
+                        pop.chase, pop.date, pop.number, pop.license, pop.id], 
+                        (err,result) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(result);
+        });
+    });
+}
+
+const modifyPhoto = (photo) => {
+    return new Promise((resolve, reject) => {
+        let sql = `UPDATE pictures SET Main_Picture = ?  WHERE FK_Pop = ?`
+        conn.query(sql, [photo.pop.photo, photo.pop.id], 
+                        (err,result) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve(result);
+        })
+    })
+}
+
 module.exports = {
     AddProduit,
     AddPicture,
     DeletePictureById,
     DeletePopById,
-    getLicense
+    getLicense,
+    getPopById,
+    modifyPop,
+    modifyPhoto
 }
